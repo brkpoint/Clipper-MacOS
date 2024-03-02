592262502915d8b0bfb0e46dc4f83e6e0a1bc160
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-03-02 18:13:29 +0100

    added icons (not fully)

---
+++ src/ApplicationMenu.swift
@@ -0,0 +1,32 @@
+import Foundation
+import SwiftUI
+
+class ApplicationMenu: NSObject {
+    let menu = NSMenu()
+    
+    func createMenu() -> NSMenu {
+        let viewMain = ContentView()
+        let topView = NSHostingController(rootView: viewMain)
+        topView.view.frame.size = CGSize(width: 225, height: 225)
+        
+        let customMenuItem = NSMenuItem()
+        customMenuItem.view = topView.view
+        menu.addItem(customMenuItem)
+        menu.addItem(NSMenuItem.separator())
+        
+        let quitMenuItem = NSMenuItem(title: "Quit",
+                                       action: #selector(quit),
+                                       keyEquivalent: "q")
+        quitMenuItem.target = self
+        menu.addItem(quitMenuItem)
+        
+        return menu
+    }
+    
+    @objc func about(sender: NSMenuItem) {
+        NSApp.orderFrontStandardAboutPanel()
+    }
+    @objc func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
+}
--- src/WinTool.swift
+++ src/WinTool.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 @main
 struct WinTool: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
+        Settings {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    static private(set) var instance: AppDelegate!
+    lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let menu = ApplicationMenu()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        AppDelegate.instance = self
+        statusBarItem.button?.image = NSImage(contentsOfFile: "./assets/256.png")
+        statusBarItem.button?.imagePosition = .imageLeading
+        statusBarItem.menu = menu.createMenu()
     }
 }
