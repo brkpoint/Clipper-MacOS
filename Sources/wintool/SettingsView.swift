7d6af786adaf4841a48a9e92700399e218461004
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-03-16 22:35:15 +0100

    some setting things added

--- Sources/wintool/SettingsView.swift
+++ Sources/wintool/SettingsView.swift
@@ -1,11 +1,52 @@
 import SwiftUI
+import HotKey
 
 struct SettingsView: View {
-    var body: some View {
-        VStack {
-            Text("hello world!")
-        }
-        .padding()
-        .frame(width: 200, height: 150)
+    private enum Tabs: Hashable {
+        case general, keybinds
     }
-} 
+
+    var body: some View {
+        TabView {
+            GeneralSettingsView()
+                .tabItem {
+                    Label("General", systemImage: "gear")
+                }
+                .tag(Tabs.general)
+            KeybindsSettingsView()
+                .tabItem {
+                    Label("Keybinds", systemImage: "keyboard.badge.ellipsis")
+                }
+                .tag(Tabs.keybinds)
+        }
+        .frame(width: 300, height: 150)
+    }
+}
+
+struct GeneralSettingsView: View {
+    @AppStorage("enableShortcuts") private var enableShortcuts = true
+    // @AppStorage("fontSize") private var fontSize = 12.0
+
+    var body: some View {
+        Form {
+            Toggle("Shortcuts", isOn: $enableShortcuts)
+        }
+        // Form {
+        //     Toggle("Show Previews", isOn: $showPreview)
+        //     Slider(value: $fontSize, in: 9...96) {
+        //         Text("Font Size (\(fontSize, specifier: "%.0f") pts)")
+        //     }
+        // }
+        // .padding(20)
+    }
+}
+
+struct KeybindsSettingsView: View {
+    //@AppStorage("shortcutsBindings") private var shortcutsBindings: Data
+
+    var body: some View {
+        Form {
+
+        }
+    }
+}
