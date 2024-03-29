22a24dbe2b9cd815e17bb2b0d591c03ee1200925
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-03-29 23:18:05 +0100

    small code fix (forgot to comment this)

--- WinTool/WinTool/Window/Snapping/SnappingManager.swift
+++ WinTool/WinTool/Window/Snapping/SnappingManager.swift
@@ -11,7 +11,7 @@ class SnappingManager {
                 return
             }
             
-            event.window?.mouseLocationOutsideOfEventStream
+            //event.window?.mouseLocationOutsideOfEventStream
             
             // to do
         }
