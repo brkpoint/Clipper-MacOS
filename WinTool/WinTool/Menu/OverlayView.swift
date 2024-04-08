b26843dd846065f822ba23b1c356a62577966188
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-04-08 22:15:32 +0200

    overlay added (doesnt work) and fixed some stuff

---
+++ WinTool/WinTool/Menu/OverlayView.swift
@@ -0,0 +1,8 @@
+//
+//  OverlayView.swift
+//  WinTool
+//
+//  Created by Aleksander on 08/04/2024.
+//
+
+import Foundation
---
+++ WinTool/WinTool/Utils/FloatingPanel.swift
@@ -0,0 +1,19 @@
+import Foundation
+import SwiftUI
+
+class FloatingPanel<Content: View>: NSPanel {
+    init(content: Content) {
+        self.content = content
+        super.init(contentRect: .zero, styleMask: [.titled, .closable, .fullSizeContentView], backing: .buffered, defer: false)
+        self.level = .floating
+        self.isOpaque = false
+        self.hasShadow = false
+        self.becomesKeyOnlyIfNeeded = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    var content: Content
+}
