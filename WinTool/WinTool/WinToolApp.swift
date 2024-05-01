89eba67da53798fd939122941191d8e8a7b3db7a
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-05-01 13:51:30 +0200

    more big changes

---
+++ WinTool/WinTool/WinToolApp.swift
@@ -0,0 +1,42 @@
+import Cocoa
+import SwiftUI
+import ServiceManagement
+import KeyboardShortcuts
+
+@main
+struct Main: App {
+    static var shared: Main = Main()
+    var environment = EnvironmentValues()
+    let appName = "WinTool"
+    let bundleIdentifier = "com.shibaofficial.WinTool"
+    let userDefaultsKey = "setting."
+    var contentView = MenuView()
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @StateObject private var appState: AppState = AppState()
+    @ObservedObject var shortcuts: Shortcuts = Shortcuts()
+    var body: some Scene {
+        Settings {
+            SettingsView()
+        }
+    }
+}
+
+@MainActor
+final class AppState: ObservableObject {
+    init() {
+        for item in ResizeType.allCases {
+            let shortcut = KeyboardShortcuts.Name.init(item.rawValue, default: .init(item.key, modifiers: [item.modifiers]))
+            Main.shared.shortcuts.list.append(Shortcut(shortcut, item.rawValue))
+            KeyboardShortcuts.onKeyDown(for: shortcut) {
+                if !SettingsManager.shared.shortcutsEnabled.value {
+                    return
+                }
+                
+                item.execute()
+            }
+        }
+
+        print("INFO: Added global shortcuts")
+    }
+}
