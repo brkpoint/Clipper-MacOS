91109b418f2c948859177822ee5ca21671fc5c69
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-05-03 00:20:55 +0200

    big update

---
+++ Clipper/Clipper/ClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipperApp.swift
+//  Clipper
+//
+//  Created by Aleksander on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
