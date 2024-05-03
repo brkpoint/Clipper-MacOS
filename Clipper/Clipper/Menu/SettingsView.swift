96f4aca7c199db60e8806b1c2b07d9af269dbed3
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-05-03 23:12:41 +0200

    code refactoring

--- Clipper/Clipper/Menu/SettingsView.swift
+++ Clipper/Clipper/Menu/SettingsView.swift
@@ -15,12 +15,14 @@ struct SettingsView: View {
             }
             .tag(Tabs.general)
             .padding(5)
+            
             KeybindsSettingsView()
             .tabItem {
                 Label("Keybinds", systemImage: "keyboard.badge.ellipsis")
             }
             .tag(Tabs.keybinds)
             .padding(5)
+            
             AppearanceSettingsView()
             .tabItem {
                 Label("Appearance", systemImage: "sparkles")
@@ -45,6 +47,7 @@ struct GeneralSettingsView: View {
                 .alignmentGuide(.leading) { d in
                     d[.leading]
                 }
+                
                 Toggle("Snapping", isOn: $snappingEnabled)
                 .onChange(of: snappingEnabled) {
                     SettingsManager.shared.shortcutsEnabled.value = snappingEnabled
@@ -52,6 +55,7 @@ struct GeneralSettingsView: View {
                 .alignmentGuide(.leading) { d in
                     d[.leading]
                 }
+                
                 LaunchAtLogin.Toggle("Toggle launch at login")
                 .alignmentGuide(.leading) { d in
                     d[.leading]
@@ -114,6 +118,7 @@ struct AppearanceSettingsView: View {
                         overlayBackgroundColor = Color(SettingsManager.shared.overlayBackgroundColor.value)
                     }
                     .frame(maxWidth: .infinity, alignment: .center)
+                    
                     VStack(alignment: .leading) {
                         Slider(value: $timeToSnap, in: 0.3 ... 1.5, step: 0.1, minimumValueLabel: Text("0.3"), maximumValueLabel: Text("1.5")) {
                             Label("Time To Snap (In Seconds)", systemImage: "timer")
@@ -125,6 +130,7 @@ struct AppearanceSettingsView: View {
                             d[.leading]
                         }
                         .padding(5)
+                        
                         Slider(value: $overlayAlpha, in: 0 ... 10, step: 0.5, minimumValueLabel: Text("1"), maximumValueLabel: Text("100")) {
                             Label("Overlay Alpha", systemImage: "circle.lefthalf.filled")
                         }
@@ -135,6 +141,7 @@ struct AppearanceSettingsView: View {
                             d[.leading]
                         }
                         .padding(5)
+                        
                         ColorPicker(selection: $overlayBorderColor, supportsOpacity: false) {
                             Label("Overlay Border Color", systemImage: "square")
                         }
@@ -146,6 +153,7 @@ struct AppearanceSettingsView: View {
                             d[.leading]
                         }
                         .padding(5)
+                        
                         ColorPicker(selection: $overlayBackgroundColor, supportsOpacity: false) {
                             Label("Overlay Background Color", systemImage: "square.fill")
                         }
@@ -160,6 +168,7 @@ struct AppearanceSettingsView: View {
                     }
                 }
                 .padding(5)
+                
                 VStack {
                     Text("Preview:")
                     VStack {
@@ -175,11 +184,11 @@ struct AppearanceSettingsView: View {
                     .cornerRadius(8)
                     .overlay() {
                         RoundedRectangle(cornerRadius: 10)
-                            .stroke(overlayBorderColor)
-                            .fill(overlayBackgroundColor)
-                            .frame(width: 180, height: 180)
-                            .opacity(Double(overlayAlpha / 10))
-                            .padding(5)
+                        .stroke(overlayBorderColor)
+                        .fill(overlayBackgroundColor)
+                        .frame(width: 180, height: 180)
+                        .opacity(Double(overlayAlpha / 10))
+                        .padding(5)
                     }
                 }
             }
